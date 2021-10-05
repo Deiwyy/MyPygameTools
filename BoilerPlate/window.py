13bd9a929ac99f2bb91acc3fb3b0e3de5c967668
Dawid Kaprol <69752072+Deiwyy@users.noreply.github.com>
2021-10-05 18:42:10 +0200

    Add files via upload

---
+++ BoilerPlate/window.py
@@ -0,0 +1,28 @@
+import pygame
+
+class Window:
+	def __init__(self, width, height, title='My Game'):
+		self.width = width
+		self.height = height
+		self.title = title
+		self.window = pygame.display.set_mode((width, height))
+
+	def get_height(self):
+		return self.height
+
+	def get_width(self):
+		return self.height
+
+	def get_size(self):
+		return (self.width, self.height)
+
+	def get(self):
+		return self.window
+
+	def draw(self, surfaces):
+		for s in surfaces:
+			self.window.blit(s, s.get_rect().x, s.get_rect().y)
+
+	def clear(self, color):
+		self.window.fill(color)
+		pygame.display.update()
